c1dd8564ec7902f757b58e9b96c9e9e0c8011d6b
Jamie Sawyer <jamie@jsawyer.co.uk>
2021-05-09 20:19:40 +0100

    Got it all working

--- compareDayValues.py
+++ compareDayValues.py
@@ -1,10 +1,12 @@
 import yfinance as yf
 import datetime as dt
 import pandas as pd
+from stonklib import returnFinraShortData
+import plotly.graph_objects as go
 
 # start date inclusive, end date exclusive
 tickerString = "GME"
-startDateString = '2021-05-05'
+startDateString = '2021-04-10'
 endDateString = '2021-05-08'
 # end of editable
 
@@ -18,11 +20,29 @@ minVolumes = []
 
 for day in [d.date() for d in pd.date_range(start=startDate, end=endDate-dt.timedelta(days=1))]:
     minVolumesYahoo = ticker.history(interval="1m", start=day, end=day+dt.timedelta(days=1))
-    minVolumes.append(minVolumesYahoo["Volume"].sum())
+    if minVolumesYahoo["Volume"].sum() != 0:
+        minVolumes.append(minVolumesYahoo["Volume"].sum())
 
 dailyYahooVolumeData = dailyYahooVolumeData.T.reset_index()
 dailyYahooVolumeData['dailyVolume'] = dailyYahooVolumeData['Volume']
 dailyYahooVolumeData = dailyYahooVolumeData.drop('Volume',1)
 dailyYahooVolumeData['minuteVolume'] = minVolumes
 
-print(dailyYahooVolumeData)
+finraData = returnFinraShortData(startDate, endDate)
+finraData = finraData[finraData['Symbol'] == tickerString]
+finraData['Date'] = pd.to_datetime(finraData['Date'], format="%Y%m%d")
+finraData['finraShort'] = finraData['ShortVolume']
+finraData['finraVolume'] = finraData['TotalVolume']
+
+finraData = finraData.drop(['Market','ShortExemptVolume','Symbol','ShortVolume','TotalVolume'],1)
+results = pd.merge(dailyYahooVolumeData, finraData, on="Date")
+print(results)
+
+fig=go.Figure()
+
+fig.add_trace(go.Line(x=results['Date'], y=results['dailyVolume'], name="Daily Yahoo Volume"))
+fig.add_trace(go.Line(x=results['Date'], y=results['minuteVolume'], name="Minute Yahoo Volume"))
+fig.add_trace(go.Line(x=results['Date'], y=results['finraShort'], name="Finra Short Volume"))
+fig.add_trace(go.Line(x=results['Date'], y=results['finraVolume'], name="Finra Total Volume"))
+
+fig.show()
